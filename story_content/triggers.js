4926bad290a270657d1e74dcaa867cf3272b7bd4
einav-gal <einav@moondigital.co.il>
2026-06-22 12:50:14 +0300

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,101 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5tZFOgLqpCT":
+        Script1();
+        break;
+      case "5j2fsdJR36h":
+        Script2();
+        break;
+      case "5qGjE5Y74GZ":
+        Script3();
+        break;
+      case "66vMXjdknC6":
+        Script4();
+        break;
+      case "5g8a9tjr3eD":
+        Script5();
+        break;
+      case "5hY5U2hi4Qf":
+        Script6();
+        break;
+      case "5YnCUC61k9I":
+        Script7();
+        break;
+      case "63yIPHGaYpN":
+        Script8();
+        break;
+      case "6PGM9zbitMt":
+        Script9();
+        break;
+      case "6nPP8CSIw2v":
+        Script10();
+        break;
+      case "63xtNMpWtwU":
+        Script11();
+        break;
+      case "6cE7sP9xCT4":
+        Script12();
+        break;
+      case "6F7kjraeNLb":
+        Script13();
+        break;
+      case "6QsOJibxfpO":
+        Script14();
+        break;
+      case "6TFqoYvAf7n":
+        Script15();
+        break;
+      case "6cfjBGeUGme":
+        Script16();
+        break;
+      case "6jI7gsIYhJU":
+        Script17();
+        break;
+      case "6j0qmVyV20N":
+        Script18();
+        break;
+      case "6Q79HwEVU35":
+        Script19();
+        break;
+      case "6R3OiTvIePQ":
+        Script20();
+        break;
+      case "69Cj1w297Tb":
+        Script21();
+        break;
+      case "6ewVmmT5a8x":
+        Script22();
+        break;
+      case "5tMNlIY0OmM":
+        Script23();
+        break;
+      case "5eQElFBVV5X":
+        Script24();
+        break;
+      case "6eZRjEN49lD":
+        Script25();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
